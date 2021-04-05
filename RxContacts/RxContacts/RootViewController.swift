bae64566d471a1392fb8a950e96dfbd0274e5105
kyungpyoda <44656036+kyungpyoda@users.noreply.github.com>
2021-04-05 15:29:55 +0900

    [feat] Create RootViewController, Coordinator

---
+++ RxContacts/RxContacts/RootViewController.swift
@@ -0,0 +1,25 @@
+//
+//  RootViewController.swift
+//  RxContacts
+//
+//  Created by 홍경표 on 2021/04/05.
+//
+
+import UIKit
+
+class RootViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        setUp()
+    }
+    
+    private func setUp() {
+        setUpUI()
+    }
+    
+    private func setUpUI() {
+        view.backgroundColor = .systemGray
+    }
+    
+}
