c602e6a316f9d999d53dc8f883716e6c852036e9
kyungpyoda <44656036+kyungpyoda@users.noreply.github.com>
2021-04-06 14:05:28 +0900

    [feat] Create models `Contact`, `ContactSection`
    - Contact는 연락처 1개를 나타내는 객체
    - ContactSection은 연락처 목록 TableView에서 Contact의 이름 첫 문자를 기준으로 각각의 section으로 나뉘는 객체

--- RxContacts/RxContacts/Model/Contact.swift
+++ RxContacts/RxContacts/Model/Contact.swift
@@ -6,16 +6,7 @@
 //
 
 import Foundation
-import RxDataSources
 
-struct ContactSection {
-    var header: String
-    var items: [Int]
-}
-
-extension ContactSection: SectionModelType {
-    init(original: ContactSection, items: [Int]) {
-        self = original
-        self.items = items
-    }
+struct Contact {
+    var fullName: String
 }
