aef462ae12eec9962b39d841dab24566daf03c7b
kyungpyoda <44656036+kyungpyoda@users.noreply.github.com>
2021-04-06 10:04:53 +0900

    [feat] Create ContactTableViewCell

---
+++ RxContacts/RxContacts/View/ContactTableViewCell.swift
@@ -0,0 +1,12 @@
+//
+//  ContactTableViewCell.swift
+//  RxContacts
+//
+//  Created by 홍경표 on 2021/04/06.
+//
+
+import UIKit
+
+final class ContactTableViewCell: UITableViewCell {
+    static var identifier: String { String(describing: Self.self) }
+}
